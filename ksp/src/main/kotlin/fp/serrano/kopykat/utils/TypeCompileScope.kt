500b476b5edf6252ad6b34e2cf0d3c537dd86e77
Pablisco <pabs87@gmail.com>
2022-09-22 13:00:45 +0100

    Make sure we map the last simple name of a ClassName

--- ksp/src/main/kotlin/fp/serrano/kopykat/utils/TypeCompileScope.kt
+++ ksp/src/main/kotlin/fp/serrano/kopykat/utils/TypeCompileScope.kt
@@ -52,7 +52,7 @@ internal class ClassCompileScope(
 
   override val typeVariableNames: List<TypeVariableName> = typeParameters.map { it.toTypeVariableName() }
   override val target: ClassName = className
-  override val mutable: ClassName = target.map { "Mutable$simpleName" }
+  override val mutable: ClassName = target.map { "Mutable$it" }
   override val properties: Sequence<KSPropertyDeclaration> = getAllProperties()
 
   override val ClassName.parameterized get() = parameterizedWhenNotEmpty(typeVariableNames)
--- ksp/src/main/kotlin/fp/serrano/kopykat/utils/kotlin/poet/KotlinPoetExtensions.kt
+++ ksp/src/main/kotlin/fp/serrano/kopykat/utils/kotlin/poet/KotlinPoetExtensions.kt
@@ -14,8 +14,8 @@ internal fun TypeCompileScope.className(first: String, vararg rest: String) =
 
 internal val KSDeclaration.className: ClassName get() = ClassName(packageName = packageName.asString(), name)
 
-internal fun ClassName.map(name: ClassName.() -> String) =
-  ClassName(packageName, name(this))
+internal fun ClassName.map(name: (String) -> String) =
+  ClassName(packageName, simpleNames.run { dropLast(1) + name(last()) })
 
 internal fun TypeCompileScope.buildFile(fileName: String, block: FileCompilerScope.() -> Unit): FileSpec =
   FileSpec.builder(packageName.asString(), fileName).also { toFileScope(it).block() }.build()
