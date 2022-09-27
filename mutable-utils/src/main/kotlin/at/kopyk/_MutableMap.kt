296bea14fad75dc05e5ca40d034304b05e99352a
Pablisco <pabs87@gmail.com>
2022-09-27 13:19:18 +0100

    Split utils into lists and maps

--- mutable-utils/src/main/kotlin/at/kopyk/_MutableMap.kt
+++ mutable-utils/src/main/kotlin/at/kopyk/_MutableMap.kt
@@ -1,28 +1,5 @@
 package at.kopyk
 
-/**
- * Applies 'transform' to each element in the list,
- * reusing the same structure to keep them.
- */
-public fun <A> MutableList<A>.mutateAll(
-  transform: (A) -> A
-): MutableList<A> = apply {
-  forEachIndexed { ix, value ->
-    this[ix] = transform(value)
-  }
-}
-
-/**
- * Applies 'transform' to each element in the list,
- * reusing the same structure to keep them.
- */
-public fun <A> MutableList<A>.mutateAllIndexed(
-  transform: (index: Int, value: A) -> A
-): MutableList<A> = apply {
-  forEachIndexed { ix, value ->
-    this[ix] = transform(ix, value)
-  }
-}
 
 /**
  * Applies 'transform' to each value in the map,
