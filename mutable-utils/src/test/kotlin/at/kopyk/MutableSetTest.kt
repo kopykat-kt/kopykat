821fb69e42b0bc24be9ea987fcd8579aebcc4b17
Pablisco <pabs87@gmail.com>
2022-09-28 01:00:29 +0100

    Add MutableSet utils

---
+++ mutable-utils/src/test/kotlin/at/kopyk/MutableSetTest.kt
@@ -0,0 +1,48 @@
+package at.kopyk
+
+import at.kopyk.utils.assumingCast
+import io.kotest.matchers.collections.shouldContainExactly
+import org.junit.jupiter.api.Test
+
+class MutableSetTest {
+
+  @Test
+  fun `can mutate all items`() = testMutation(
+    given = mutableSetOf("a", "b", "c"),
+    whenWe = { mutateAll { it + it } },
+    then = { it shouldContainExactly listOf("aa", "bb", "cc") },
+  )
+
+  @Test
+  fun `can mutate all items indexed`() = testMutation(
+    given = mutableSetOf("a", "b", "c"),
+    whenWe = { mutateAllIndexed { index, value -> value + index } },
+    then = { it shouldContainExactly listOf("a0", "b1", "c2") },
+  )
+
+  @Test
+  fun `removes null mutations`() = testMutation(
+    given = mutableSetOf("a", "b", "c"),
+    whenWe = { mutateAllNotNull { value -> value.takeUnless { it == "b" } } },
+    then = { it shouldContainExactly listOf("a", "c") },
+  )
+
+  @Test
+  fun `removes null mutations with index`() = testMutation(
+    given = mutableSetOf("a", "b", "c"),
+    whenWe = {
+      mutateAllNotNullIndexed { index, value ->
+        value.takeUnless { it == "b" }?.plus("$index")
+      }
+    },
+    then = { it shouldContainExactly listOf("a0", "c2") }
+  )
+
+  @Test
+  fun `keep instances of a given type`() = testMutation(
+    given = mutableSetOf("a", 10, "c"),
+    whenWe = assumingCast { removeUnlessInstanceOf<String>().mutateAll { it + it } },
+    then = { it shouldContainExactly listOf("aa", "cc") }
+  )
+
+}
