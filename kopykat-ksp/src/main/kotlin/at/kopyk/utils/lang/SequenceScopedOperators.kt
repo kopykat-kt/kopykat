59ba4f87aa0ac3e02ed188c8d201caff0f811454
Pablisco <pabs87@gmail.com>
2022-10-15 00:43:53 +0100

    Add operator to filter instance with a predicate

--- kopykat-ksp/src/main/kotlin/at/kopyk/utils/lang/SequenceScopedOperators.kt
+++ kopykat-ksp/src/main/kotlin/at/kopyk/utils/lang/SequenceScopedOperators.kt
@@ -16,3 +16,9 @@ internal fun <A> Sequence<A>.onEachRun(block: A.() -> Unit) = onEach(block)
  * Alias of [map] with the item as the receiver.
  */
 internal fun <A, R> Sequence<A>.mapRun(block: A.() -> R) = map(block)
+
+/**
+ * Filters instances of [T] as long as they pass the provided [predicate]
+ */
+internal inline fun <reified T> Sequence<*>.filterIsInstance(noinline predicate: T.() -> Boolean): Sequence<T> =
+  filterIsInstance<T>().filter(predicate)
