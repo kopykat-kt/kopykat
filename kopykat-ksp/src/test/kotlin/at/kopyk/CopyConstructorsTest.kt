bef02aaa9c23be405a112813fe285f7215e60469
Pablisco <pabs87@gmail.com>
2022-10-16 21:55:46 +0100

    Added more test cases

--- kopykat-ksp/src/test/kotlin/at/kopyk/CopyConstructorsTest.kt
+++ kopykat-ksp/src/test/kotlin/at/kopyk/CopyConstructorsTest.kt
@@ -37,6 +37,22 @@ class CopyConstructorsTest {
   }
 
   @Test
+  fun `data class @CopyFrom class`() {
+    """
+      |import at.kopyk.CopyFrom
+      |
+      |class Person(val name: String, val age: Int)
+      |
+      |@CopyFrom(Person::class)
+      |data class LocalPerson(val name: String, val age: Int)
+      |
+      |val p1 = Person("Alex", 1)
+      |val p2 = LocalPerson(p1)
+      |val r = p2.age
+      """.evals("r" to 1)
+  }
+
+  @Test
   fun `data class @CopyTo data class`() {
     """
       |import at.kopyk.CopyTo
@@ -65,6 +81,73 @@ class CopyConstructorsTest {
       |val p1 = LocalPerson("Alex", 1)
       |val p2 = Person(p1)
       |val r = p2.age
+      """.evals("r" to 1)
+  }
+
+  @Test
+  fun `data class @CopyTo class`() {
+    """
+      |import at.kopyk.CopyTo
+      |
+      |class Person(val name: String, val age: Int)
+      |
+      |@CopyTo(Person::class)
+      |data class LocalPerson(val name: String, val age: Int)
+      |
+      |val p1 = LocalPerson("Alex", 1)
+      |val p2 = Person(p1)
+      |val r = p2.age
+      """.evals("r" to 1)
+  }
+
+  @Test
+  fun `data class @Copy data class`() {
+    """
+      |import at.kopyk.Copy
+      |
+      |data class Person(val name: String, val age: Int)
+      |
+      |@Copy(Person::class)
+      |data class LocalPerson(val name: String, val age: Int)
+      |
+      |val p1 = LocalPerson("Alex", 1)
+      |val p2 = Person(p1)
+      |val p3 = LocalPerson(p2)
+      |val r = p3.age
+      """.evals("r" to 1)
+  }
+
+  @Test
+  fun `class @Copy data class`() {
+    """
+      |import at.kopyk.Copy
+      |
+      |data class Person(val name: String, val age: Int)
+      |
+      |@Copy(Person::class)
+      |class LocalPerson(val name: String, val age: Int)
+      |
+      |val p1 = LocalPerson("Alex", 1)
+      |val p2 = Person(p1)
+      |val p3 = LocalPerson(p2)
+      |val r = p3.age
+      """.evals("r" to 1)
+  }
+
+  @Test
+  fun `data class @Copy class`() {
+    """
+      |import at.kopyk.Copy
+      |
+      |class Person(val name: String, val age: Int)
+      |
+      |@Copy(Person::class)
+      |data class LocalPerson(val name: String, val age: Int)
+      |
+      |val p1 = LocalPerson("Alex", 1)
+      |val p2 = Person(p1)
+      |val p3 = LocalPerson(p2)
+      |val r = p3.age
       """.evals("r" to 1)
   }
 
