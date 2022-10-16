845174f40cd6db883dc485900f49329df4a1d78f
Pablisco <pabs87@gmail.com>
2022-10-16 23:17:45 +0100

    Added a lot more test cases

--- kopykat-ksp/src/test/kotlin/at/kopyk/CopyConstructorsTest.kt
+++ kopykat-ksp/src/test/kotlin/at/kopyk/CopyConstructorsTest.kt
@@ -4,152 +4,175 @@ import org.junit.jupiter.api.Test
 
 class CopyConstructorsTest {
 
-  @Test
-  fun `data class @CopyFrom data class`() {
-    """
+  private fun copyFrom(annotatedModifiers: String, targetModifiers: String) {
+    if (annotatedModifiers == "value" || targetModifiers == "value") {
+      """
       |import at.kopyk.CopyFrom
       |
-      |data class Person(val name: String, val age: Int)
+      |${if (targetModifiers == "value") "@JvmInline" else ""}
+      |$targetModifiers class Person(val name: String)
       |
       |@CopyFrom(Person::class)
-      |data class LocalPerson(val name: String, val age: Int)
+      |${if (annotatedModifiers == "value") "@JvmInline" else ""}
+      |$annotatedModifiers class LocalPerson(val name: String)
+      |
+      |val p1 = Person("Alex")
+      |val p2 = LocalPerson(p1)
+      |val r = p2.name
+      """.evals("r" to "Alex")
+    } else {
+      """
+      |import at.kopyk.CopyFrom
+      |
+      |$targetModifiers class Person(val name: String, val age: Int)
+      |
+      |@CopyFrom(Person::class)
+      |$annotatedModifiers class LocalPerson(val name: String, val age: Int)
       |
       |val p1 = Person("Alex", 1)
       |val p2 = LocalPerson(p1)
       |val r = p2.age
       """.evals("r" to 1)
+    }
   }
 
   @Test
-  fun `class @CopyFrom data class`() {
-    """
-      |import at.kopyk.CopyFrom
-      |
-      |data class Person(val name: String, val age: Int)
-      |
-      |@CopyFrom(Person::class)
-      |class LocalPerson(val name: String, val age: Int)
-      |
-      |val p1 = Person("Alex", 1)
-      |val p2 = LocalPerson(p1)
-      |val r = p2.age
-      """.evals("r" to 1)
-  }
+  fun `class @CopyFrom class`() = copyFrom("", "")
 
   @Test
-  fun `data class @CopyFrom class`() {
-    """
-      |import at.kopyk.CopyFrom
-      |
-      |class Person(val name: String, val age: Int)
-      |
-      |@CopyFrom(Person::class)
-      |data class LocalPerson(val name: String, val age: Int)
-      |
-      |val p1 = Person("Alex", 1)
-      |val p2 = LocalPerson(p1)
-      |val r = p2.age
-      """.evals("r" to 1)
-  }
+  fun `data class @CopyFrom data class`() = copyFrom("data", "data")
 
   @Test
-  fun `data class @CopyTo data class`() {
-    """
+  fun `class @CopyFrom data class`() = copyFrom("", "data")
+
+  @Test
+  fun `data class @CopyFrom class`() = copyFrom("data", "")
+
+  @Test
+  fun `value class @CopyFrom data class`() = copyFrom("value", "data")
+
+  @Test
+  fun `value class @CopyFrom class`() = copyFrom("value", "")
+
+  @Test
+  fun `data class @CopyFrom value class`() = copyFrom("data", "value")
+
+  @Test
+  fun `class @CopyFrom value class`() = copyFrom("", "value")
+
+  private fun copyTo(annotatedModifiers: String, targetModifiers: String) {
+    if (annotatedModifiers == "value" || targetModifiers == "value") {
+      """
       |import at.kopyk.CopyTo
       |
-      |data class Person(val name: String, val age: Int)
+      |${if (targetModifiers == "value") "@JvmInline" else ""}
+      |$targetModifiers class Person(val name: String)
       |
       |@CopyTo(Person::class)
-      |data class LocalPerson(val name: String, val age: Int)
+      |${if (annotatedModifiers == "value") "@JvmInline" else ""}
+      |$annotatedModifiers class LocalPerson(val name: String)
+      |
+      |val p1 = LocalPerson("Alex")
+      |val p2 = Person(p1)
+      |val r = p2.name
+      """.evals("r" to "Alex")
+    } else {
+      """
+      |import at.kopyk.CopyTo
+      |
+      |$targetModifiers class Person(val name: String, val age: Int)
+      |
+      |@CopyTo(Person::class)
+      |$annotatedModifiers class LocalPerson(val name: String, val age: Int)
       |
       |val p1 = LocalPerson("Alex", 1)
       |val p2 = Person(p1)
       |val r = p2.age
       """.evals("r" to 1)
+    }
   }
 
   @Test
-  fun `class @CopyTo data class`() {
-    """
-      |import at.kopyk.CopyTo
-      |
-      |data class Person(val name: String, val age: Int)
-      |
-      |@CopyTo(Person::class)
-      |class LocalPerson(val name: String, val age: Int)
-      |
-      |val p1 = LocalPerson("Alex", 1)
-      |val p2 = Person(p1)
-      |val r = p2.age
-      """.evals("r" to 1)
-  }
+  fun `class @CopyTo class`() = copyTo("", "")
 
   @Test
-  fun `data class @CopyTo class`() {
-    """
-      |import at.kopyk.CopyTo
-      |
-      |class Person(val name: String, val age: Int)
-      |
-      |@CopyTo(Person::class)
-      |data class LocalPerson(val name: String, val age: Int)
-      |
-      |val p1 = LocalPerson("Alex", 1)
-      |val p2 = Person(p1)
-      |val r = p2.age
-      """.evals("r" to 1)
-  }
+  fun `data class @CopyTo data class`() = copyTo("data", "data")
 
   @Test
-  fun `data class @Copy data class`() {
-    """
+  fun `class @CopyTo data class`() = copyTo("", "data")
+
+  @Test
+  fun `data class @CopyTo class`() = copyTo("data", "")
+
+  @Test
+  fun `value class @CopyTo data class`() = copyTo("value", "data")
+
+  @Test
+  fun `value class @CopyTo class`() = copyTo("value", "")
+
+  @Test
+  fun `data class @CopyTo value class`() = copyTo("data", "value")
+
+  @Test
+  fun `class @CopyTo value class`() = copyTo("", "value")
+
+  private fun copy(annotatedModifiers: String, targetModifiers: String) {
+    if (annotatedModifiers == "value" || targetModifiers == "value") {
+      """
       |import at.kopyk.Copy
       |
-      |data class Person(val name: String, val age: Int)
+      |${if (targetModifiers == "value") "@JvmInline" else ""}
+      |$targetModifiers class Person(val name: String)
       |
       |@Copy(Person::class)
-      |data class LocalPerson(val name: String, val age: Int)
+      |${if (annotatedModifiers == "value") "@JvmInline" else ""}
+      |$annotatedModifiers class LocalPerson(val name: String)
+      |
+      |val p1 = LocalPerson("Alex")
+      |val p2 = Person(p1)
+      |val p3 = LocalPerson(p2)
+      |val r = p3.name
+      """.evals("r" to "Alex")
+    } else {
+      """
+      |import at.kopyk.Copy
+      |
+      |$targetModifiers class Person(val name: String, val age: Int)
+      |
+      |@Copy(Person::class)
+      |$annotatedModifiers class LocalPerson(val name: String, val age: Int)
       |
       |val p1 = LocalPerson("Alex", 1)
       |val p2 = Person(p1)
       |val p3 = LocalPerson(p2)
       |val r = p3.age
       """.evals("r" to 1)
+    }
   }
 
   @Test
-  fun `class @Copy data class`() {
-    """
-      |import at.kopyk.Copy
-      |
-      |data class Person(val name: String, val age: Int)
-      |
-      |@Copy(Person::class)
-      |class LocalPerson(val name: String, val age: Int)
-      |
-      |val p1 = LocalPerson("Alex", 1)
-      |val p2 = Person(p1)
-      |val p3 = LocalPerson(p2)
-      |val r = p3.age
-      """.evals("r" to 1)
-  }
+  fun `class @Copy class`() = copy("", "")
 
   @Test
-  fun `data class @Copy class`() {
-    """
-      |import at.kopyk.Copy
-      |
-      |class Person(val name: String, val age: Int)
-      |
-      |@Copy(Person::class)
-      |data class LocalPerson(val name: String, val age: Int)
-      |
-      |val p1 = LocalPerson("Alex", 1)
-      |val p2 = Person(p1)
-      |val p3 = LocalPerson(p2)
-      |val r = p3.age
-      """.evals("r" to 1)
-  }
+  fun `data class @Copy data class`() = copy("data", "data")
+
+  @Test
+  fun `class @Copy data class`() = copy("", "data")
+
+  @Test
+  fun `data class @Copy class`() = copy("data", "")
+
+  @Test
+  fun `value class @Copy data class`() = copy("value", "data")
+
+  @Test
+  fun `value class @Copy class`() = copy("value", "")
+
+  @Test
+  fun `data class @Copy value class`() = copy("data", "value")
+
+  @Test
+  fun `class @Copy value class`() = copy("", "value")
 
   @Test
   fun `@Copy, missing field should fail compilation`() {
