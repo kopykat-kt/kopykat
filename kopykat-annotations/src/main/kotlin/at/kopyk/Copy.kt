ed837cc54f1fb1a4748079aae44da4eb2b51112d
Pablisco <pabs87@gmail.com>
2022-10-28 10:45:59 +0100

    Merge Copy annotation to a single file

--- kopykat-annotations/src/main/kotlin/at/kopyk/Copy.kt
+++ kopykat-annotations/src/main/kotlin/at/kopyk/Copy.kt
@@ -12,3 +12,25 @@ import kotlin.reflect.KClass
 @Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS)
 @Retention(AnnotationRetention.SOURCE)
 public annotation class Copy(val type: KClass<*>)
+
+/**
+ * Marks type or typealias to generate copy constructor from
+ * the provided [type] to the subject of this annotation.
+ *
+ * Both the subject of this annotation and the provided type
+ * have to have the same constructor properties.
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS)
+@Retention(AnnotationRetention.SOURCE)
+public annotation class CopyFrom(val type: KClass<*>)
+
+/**
+ * Marks type or typealias to generate copy constructor from
+ * the subject of this annotation to the provided [type].
+ *
+ * Both the subject of this annotation and the provided type
+ * have to have the same constructor properties.
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS)
+@Retention(AnnotationRetention.SOURCE)
+public annotation class CopyTo(val type: KClass<*>)
